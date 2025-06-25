e96619eb34198bb6ad9ad05a5ab8684bdb4f4cfa
Aparnagopal <gopal_aparna@yahoo.com>
2025-06-25 14:01:23 +0530

    form ID corrected

--- api/webhook/form-submission.js
+++ api/webhook/form-submission.js
@@ -1,7 +1,7 @@
 // Form configuration mapping - Add your forms here
 const FORM_CONFIGS = {
   /// Crowd Funding Donor comments form
-  "684184cc1e7d70808e8d1b72": {
+  "6852c7791d84f17a29ace6f0": {
     name: "Student Crowd Funding Donor Comments",
     collectionId: process.env.WEBFLOW_DONOR_COMMENTS_COLLECTION_ID,
     fieldMapping: {
